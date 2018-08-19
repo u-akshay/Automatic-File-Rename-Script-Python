73ce9a8dd6034e4e7a103949839aa0e4a2203000
shakirck <shakirck333#gmail.com>
2018-08-19 18:53:11 +0530

    Version 1 has been created

---
+++ rename_script.py
@@ -0,0 +1,26 @@
+import os
+
+os.chdir('F:\Tutorial\python\chorey shafer\python\Flask') # change the dirictory here 
+for file in os.listdir():
+	#print(os.path.splitext(file))
+	f_name,f_ext = os.path.splitext(file)
+	p_name,p_name_1,v_name_num,v_name,v_id = f_name.split('-')
+	video_name,video_num = v_name_num.split('Part')
+    
+	video_name = video_name.strip()
+	video_num = video_num.strip().zfill(2)
+	p_name = p_name.strip()
+	p_name_1 = p_name_1.strip()
+	v_name_num = v_name_num.strip()
+	v_name = v_name.strip()
+	v_id = v_id.strip()
+	
+	newFile = '{}-{}.{}'.format(video_num,v_name,f_ext)
+
+	os.rename(file,newFile)
+
+# i have a problems with 
+
+
+
+
